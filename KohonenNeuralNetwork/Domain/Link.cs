3ccc042bb3561a6a7ce5c7fd64c3af275c70422a
hartigans <hartigans@live.com>
2013-02-07 21:43:31 +0300

    init solution and Domain project

---
+++ KohonenNeuralNetwork/Domain/Link.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain
+{
+	public class Link
+	{
+		public Neuron Neuron { get; set; }
+
+		public double Weight { get; set; }
+	}
+}
